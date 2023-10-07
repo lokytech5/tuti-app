f4b5908c86175f4cabf3e71272de196eaba9d828
lokytech5 <lokytech5@gmail.com>
2023-10-07 05:10:03 +0100

    Fix design bug for small screen size

--- app/home/ProductDescription.tsx
+++ app/home/ProductDescription.tsx
@@ -7,12 +7,12 @@ const ProductDescription = () => {
   return (
     <section className="bg-gradient-to-r from-primary to-blue-600 py-4 md:py-12"> 
     <div className="flex justify-center mt-2 md:mt-4">
-        <div className="artboard artboard-horizontal phone-3 sm:min-h-40 bg-white p-4 md:p-8 rounded-lg shadow-lg flex flex-col md:flex-row"> 
+        <div className="artboard artboard-horizontal lg:h-auto sm:min-h-40 bg-white p-4 md:p-8 rounded-lg shadow-lg flex flex-col md:flex-row"> 
         
             
             {/* Placeholder for Image */}
-            <div className="flex-none w-full md:w-1/2 mt-8 md:mt-14 md:mb-0 md:mr-8">
-                <div className="w-full h-56 md:h-96 rounded overflow-hidden">
+            <div className="flex-none w-full md:w-1/2 mt-4 md:mt-14 md:mb-0 md:mr-8 lg:-mt-0">
+            <div className="w-full h-48 lg:h-80 max-h-1/2 md:max-h-full rounded overflow-hidden">
                     <figure className="overflow-hidden rounded-t-lg">
                         <Image src={pic1} alt="woman with hair" width={284} height={213} layout="responsive" className="object-cover" />
                     </figure>
@@ -21,8 +21,8 @@ const ProductDescription = () => {
 
             {/* Description */}
             <div className="flex flex-col justify-center space-y-3 md:space-y-4 w-full md:w-1/2">
-                <div className="flex items-center space-x-1 md:space-x-2 mb-2 md:mb-4">
-                    <FaInfoCircle className="text-primary w-5 h-5 md:w-6 md:h-6" />
+                <div className="flex items-center space-x-1 md:space-x-2 mb-2 md:mb-4 mt-2 ml-10 mb:ml-20 lg:ml-60">
+                    <FaInfoCircle className="text-primary w-5 mt-2 h-5 md:w-6 md:h-6" />
                     <span className="badge badge-info">Featured Product</span>
                 </div>
                 <h2 className="text-lg md:text-xl font-semibold text-center text-gray-600 mb-1 md:mb-2 sm:mb-12">Elevate Your Beauty</h2>
