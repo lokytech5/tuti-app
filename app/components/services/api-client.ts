49468a9c040fbc8492880b36cb680ce774275308
lokytech5 <lokytech5@gmail.com>
2023-11-25 08:51:46 +0100

    conditionally applying authorization Header to apiClient.

--- app/components/services/api-client.ts
+++ app/components/services/api-client.ts
@@ -1,7 +1,20 @@
 import axios from "axios";
+import { config } from "process";
 
 const apiClient = axios.create({
     baseURL: 'http://localhost:5001/api'
-})
+});
+
+apiClient.interceptors.request.use((config) => {
+    if (config.headers.applyAuthToken) {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
 export default apiClient;
