b8f1896a55112c388e799c9f772c625ed7b4ceb8
lokytech5 <lokytech5@gmail.com>
2024-06-12 09:53:26 +0100

    update url

--- app/components/services/api-client.ts
+++ app/components/services/api-client.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import { config } from "process";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'http://13.60.48.95:8000/api'
 });
 
 // Authenticated client
 const authApiClient = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'http://13.60.48.95:8000/api'
 });
 
 authApiClient.interceptors.request.use((config) => {
